52f1ef4aacd0c76b5ebf438aed68e16589f0b421
Serj-Y <126787294+Serj-Y@users.noreply.github.com>
2024-11-14 16:20:18 +0200

    Refactoring

--- src/services/geolocation/geolocationPermission.ts
+++ src/services/geolocation/geolocationPermission.ts
@@ -1,20 +1,10 @@
-// import Geolocation from 'react-native-geolocation-service';
 import * as Location from 'expo-location';
-import Platform from 'react-native';
 
 type geolocationPermissionProps = {
   setLocationPermission: (value: string) => void;
 };
 
-export const GeolocationPermission = async ({
-  setLocationPermission,
-}: geolocationPermissionProps) => {
-  const runOnPlatform = Platform.Platform.OS;
-  if (runOnPlatform === 'ios') {
+export const GeolocationPermission = async ({setLocationPermission}: geolocationPermissionProps) => {
     const permission = await Location.requestForegroundPermissionsAsync()
     setLocationPermission( permission.status);
-  } else {
-    const permission = await Location.requestForegroundPermissionsAsync()
-    setLocationPermission( permission.status);
-  }
 };
